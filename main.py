9a7edaa0998e37d717351bab195c67a731460e4e
jeeva-eng <jeevanandhan127@gmail.com>
2026-02-21 13:47:54 +0530

    Model Trainer Added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.mlops_project import logger
 from src.mlops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlops_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlops_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-
+from src.mlops_project.pipeline.stage_04_model_trainer import ModekTrainerTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -32,6 +32,17 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModekTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/mlops_project/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,27 @@
+from src.mlops_project.config.configuration import ConfigurationManager
+from src.mlops_project.components.model_trainer import ModelTrainer
+from src.mlops_project import logger
+from pathlib import Path
+
+
+STAGE_NAME="Model Trainer Stage"
+
+class ModekTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+         config=ConfigurationManager()
+         model_trainer_config=config.get_model_trainer_config()
+         model_trainer_config=ModelTrainer(config=model_trainer_config)
+         model_trainer_config.train()
+
+if __name__ =='__main__':
+    try:
+        logger.info(f">>>> syge{STAGE_NAME} started <<<<<")
+        obj=ModekTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx========x")
+    except Exception as e:
+        logger.exception(0)
+    raise e            
